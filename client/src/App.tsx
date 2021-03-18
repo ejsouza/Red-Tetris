82a1017d0a9bfc7df2da62bb02eac2319175221d
Eder De Souza <esouza@student.42.fr>
2021-03-18 15:17:37 +0100

    Update app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Header } from './components/header';
+import { Main } from './components/main';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import styled, { DefaultTheme } from 'styled-components';
 
+const Test = styled.div`
+  text-align: center;
+`
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-     <h1>ReadTetris</h1>
-    </div>
+   <>
+      <Header />
+      <Jumbotron>
+        <Container>
+          <h1 className="display-2 text-center">Welcome to <span className="bg-danger text-white">Red</span>Tetris</h1>
+          </Container>
+        </Jumbotron>
+        <Main />
+
+   </>
   );
 }
 
