45cedc8b20b8ef2081a49767424b2b595428dc59
Eder De Souza <esouza@student.42.fr>
2021-03-19 11:29:05 +0100

    Create Navigation bar

---
+++ client/components/NavigationBar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+
+const NavigationBar = () => {
+  return (
+    <>
+      <nav>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home" className="mr-auto">
+            RedTetris
+          </Navbar.Brand>
+          <Button variant="outline-info">Search</Button>
+        </Navbar>
+      </nav>
+    </>
+  );
+};
+
+export default NavigationBar;
