4c945f824da44a953bc45b0e93a61d718d2ba864
Eder De Souza <esouza@student.42.fr>
2021-03-20 11:42:02 +0100

    Update new game

--- client/components/NewGame.tsx
+++ client/components/NewGame.tsx
@@ -1,14 +1,33 @@
 import React, { useState } from 'react';
+import {useRouter} from 'next/router';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 // import ModalHeader from 'react-bootstrap/ModalHeader';
 
 const NewGame = () => {
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
+  const [show, setShow] = useState<boolean>(false);
+  const [roomName, setRoomName] = useState<string>('');
+  const [userName, setUserName] = useState<string>('');
+  const router = useRouter();
+	
+	const handleClose = (): void => {
+    setRoomName('');
+    setUserName('');
+    setShow(false);
+  };
 
+  const handSubmit = (): void => {
+    console.log(roomName);
+    // setRoomName('');
+    // setUserName('');
+    setShow(false);
+		// if all goes well redirect user to localhost:300/#<roomName>[<userName>]
+		router.push(`/about`, `/#${roomName}[${userName}]`);
+		// window.location.href = `/#${roomName}/${userName}`
+  };
   return (
     <>
       <Modal
@@ -20,18 +39,59 @@ const NewGame = () => {
         centered
         backdrop="static"
         onHide={handleClose}
-        keyboard={false}
       >
-        <Modal.Header closeButton>Enter a room and user name</Modal.Header>
+        <Modal.Header closeButton>
+          <h3 className="font-weight-light">
+            Enter a <span className="font-weight-normal">room</span> and{" "}
+            <span className="font-weight-normal">user</span>{" "}name
+          </h3>
+        </Modal.Header>
         <Modal.Body>
-          I will not close if you click outside me. Don't even try to press
-          escape key.
+          <Form>
+            <Form.Group controlId="roomName">
+              <Form.Label>Room</Form.Label>
+              <Form.Control
+                type="text"
+                isValid={roomName.length >= 4 ? true : false}
+                isInvalid={roomName.length < 4 ? true : false}
+                placeholder="Enter room"
+                onChange={(event: React.ChangeEvent<HTMLInputElement>): void =>
+                  setRoomName(event.target.value)
+                }
+              />
+              <Form.Text className="text-muted">
+                Room should not be already in use and at least four characters
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="userName">
+              <Form.Label>User</Form.Label>
+              <Form.Control
+                type="text"
+                isValid={userName.length >= 4 ? true : false}
+                isInvalid={userName.length < 4 ? true : false}
+                placeholder="Enter user name"
+                onChange={(event: React.ChangeEvent<HTMLInputElement>): void =>
+                  setUserName(event.target.value)
+                }
+              />
+              <Form.Text className="text-muted">
+                User name should be at least four characters long
+              </Form.Text>
+            </Form.Group>
+          </Form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Cancel
           </Button>
-          <Button variant="primary">Start &#8594;</Button>
+          <Button
+            variant="primary"
+            onClick={handSubmit}
+            disabled={roomName.length < 4 || userName.length < 4 ? true : false}
+          >
+            Start &#8594;
+          </Button>
         </Modal.Footer>
       </Modal>
 
@@ -43,3 +103,7 @@ const NewGame = () => {
 };
 
 export default NewGame;
+function as ( arg0: string, as: any, arg2: string ) {
+	throw new Error( 'Function not implemented.' );
+}
+
