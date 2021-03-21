6c637eb1559f4b7670ac226bc1662dae77b14553
Eder De Souza <esouza@student.42.fr>
2021-03-21 15:19:30 +0100

    Update create game

--- client/core/createGame.ts
+++ client/core/createGame.ts
@@ -1,8 +1,26 @@
-import APIurl from '../utils/const';
+import { APIurl } from '../utils/const';
 
-const createGame = (url: string): string => {
+interface IRoom {
+  id: string;
+  name: string;
+  open: boolean;
+  close: boolean;
+  numberPeopleInRoom: number;
+  players: Array<string>;
+}
+
+export const getRooms = async (): Promise<IRoom[]> => {
+  const res = await fetch(`${APIurl}/rooms`, {
+    method: 'GET', // *GET(get is optional), POST, PUT, DELETE, etc
+    mode: 'cors', // no-cors, *cors, same-origin
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return res.json();
+};
+
+export const createGame = (url: string): string => {
   console.log(`createGame -: ${url}`);
   return 'promise';
 };
-
-export default createGame;
