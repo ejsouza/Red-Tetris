4ac40bc07b4f8ab42d8c90c8595298994bb746d7
Eder De Souza <esouza@student.42.fr>
2021-03-22 21:23:27 +0100

    Create Footer component

---
+++ client/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <hr />
+        <span>I'm here to stay (Footer)</span>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
