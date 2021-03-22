2bafea253fee7452082ecba3e4d5a17300bd86b4
Eder De Souza <esouza@student.42.fr>
2021-03-22 22:53:21 +0100

    Create new function for fetching data by name

--- server/src/controllers/rooms/rooms.ts
+++ server/src/controllers/rooms/rooms.ts
@@ -2,25 +2,38 @@ import { Response, Request, NextFunction } from 'express';
 import { Model } from 'mongoose';
 import { IBaseRoom } from '../../rooms/room.interface';
 // import Room from '../../models/model';
-import Roomy from '../../models/Room';
+import Room from '../../models/Room';
 
 const getRooms = async (req: Request, res: Response): Promise<void> => {
-		await Roomy.find(function (err, rooms) {
-			if (err) {
-				console.log(`Error retrieving rooms -: ${err}`);
-				res.status(500).json({success: false, msg: `Failed to get rooms`})
-			} else {
-				res.status(200).json({success: true, rooms})
-			}
-		});
+		await Room.find(function (err, rooms) {
+      if (err) {
+        console.log(`Error retrieving rooms -: ${err}`);
+        res.status(500).json({ success: false, msg: `Failed to get rooms` });
+      } else {
+        res.status(200).json({ success: true, rooms });
+      }
+    });
 }
+
+const getRoomsByName = async (req: Request, res: Response): Promise<void> => {
+  console.log(`API  getRoomsByName -: ${req.params}`);
+  const name = req.params.name;
+  await Room.findOne({ name: name }, function (err: any, room: IBaseRoom) {
+    if (err) {
+      console.log(`Error retrieving rooms -: ${err}`);
+      res.status(500).json({ success: false, msg: `Failed to get rooms` });
+    } else {
+      res.status(200).json({ success: true, room });
+    }
+  });
+};
 
 const createRoom = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 	console.log(`createRoom: ${req.body.name}`);
 	const room: IBaseRoom = req.body;
 	console.log(`test -: ${room.players}`);
 	const request: Request = req;
-	const newRoom = new Roomy(room);
+	const newRoom = new Room(room);
 	try {
 		const room = await newRoom.save();
 		res.status(200).json({success: true})
@@ -43,4 +56,4 @@ const createRoom = async (req: Request, res: Response, next: NextFunction): Prom
 	// 	throw error;
 	// }
 } 
-export { getRooms, createRoom };
+export { getRooms, getRoomsByName, createRoom };
