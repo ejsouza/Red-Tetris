42ee9d75e9cf14249bc2e9bca17683d9663a22fd
Eder De Souza <esouza@student.42.fr>
2021-03-23 22:10:39 +0100

    Update rooms model

--- server/src/models/model.ts
+++ server/src/models/model.ts
@@ -2,25 +2,28 @@ import { IBaseRoom, IRoom } from '../rooms/room.interface';
 import { model, Schema } from 'mongoose';
 
 const roomSchema: Schema = new Schema(
-	{
-		name: {
-			type: String,
-			required: true,
-		},
-		open: {
-			type: Boolean,
-			required: true,
-		},
-		close: {
-			type: Boolean,
-			required: true,
-		},
-		numberPeopleInRoom: {
-			type: Number,
-			required: true
-		}
-	},
-	{timestamps: true}
-)
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    open: {
+      type: Boolean,
+      required: true,
+    },
+    close: {
+      type: Boolean,
+      required: true,
+    },
+    numberPeopleInRoom: {
+      type: Number,
+      required: true,
+    },
+    players: {
+      type: [String],
+    },
+  },
+  { timestamps: true }
+);
 
 export default model<IBaseRoom>('Room', roomSchema);
