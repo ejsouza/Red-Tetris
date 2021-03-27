77d2b57379f3ebde8553c7ec6f815f51958935a3
Eder De Souza <esouza@student.42.fr>
2021-03-27 11:53:26 +0100

    Update parseUrlWithHash

--- client/utils/parseUrlWithHash.ts
+++ client/utils/parseUrlWithHash.ts
@@ -1,15 +1,13 @@
 const parseUrlWithHash = (url: string): string[] => {
-  const startSeparator = url.indexOf('[');
-  const endSeparator = url.indexOf(']');
+	const path = url.startsWith('/') ? url.slice(1, url.length) : url;
+  const startSeparator = path.indexOf('[');
+  const endSeparator = path.indexOf(']');
   /**
    * When url entered through the form the url starts with '/'
    * When url entered directly on the browser url starts with '#'
    */
-  const startAt = url.startsWith('/') ? 2 : 1;
-  const incrementStartSeparator = startAt === 2 ? 0 : 1;
-  const roomName = url.slice(startAt, startSeparator);
-  const userName = url.slice(startSeparator, endSeparator);
-  console.log(`in parseUrl: ${roomName}--${userName}`);
+  const roomName = path.slice(1, startSeparator);
+  const userName = path.slice(startSeparator + 1, endSeparator);
   return [roomName, userName];
 };
 
