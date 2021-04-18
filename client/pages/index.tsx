b5568656019bc2294a0c739bf13aa203d9845209
Eder De Souza <esouza@student.42.fr>
2021-04-18 10:36:04 +0200

    Update landing page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import io from 'socket.io-client';
 import Layout from '../components/Layout';
+
 import HeadLanding from '../components/HeadLanding';
 import Menu from '../components/Menu';
 import { getRooms, createGame } from '../core/rooms';
 import parseUrlWithHash from '../utils/parseUrlWithHash';
+import { APIurl, BASEurl } from '../utils/const';
 
 interface IRoom {
   id: string;
@@ -15,11 +18,23 @@ interface IRoom {
   players: Array<string>;
 }
 
+let socket;
+
 const Index = () => {
   const [hideMenu, setHideMenu] = useState(false);
   const router = useRouter();
   const url = router.asPath;
+  const reg = /^#+[a-z]+[a-z0-9]{3,}\[[a-z]+[a-z0-9]{4,}\]/gi;
 
+  useEffect(() => {
+    socket = io(BASEurl, {
+      query: {
+        x: 42,
+      },
+    });
+    socket.emit('join', { name: 'username', room: 'roomname' });
+    console.log(socket);
+  }, []);
   useEffect(() => {
     if (url.includes('#')) {
       /**
@@ -29,15 +44,31 @@ const Index = () => {
        *
        * */
 
+      /**
+       * Use reg.test(url) here to check if it is right formated
+       * before preceding.
+       */
+
+      // remove leading '/' from url before testing
+      if (reg.test(url.slice(1))) {
+        console.log(`URL FORMAT RIGHT ${url}`);
+      } else {
+        console.log(`URL WRONG FORMAT  ${url}`);
+      }
       const [roomName, userName] = parseUrlWithHash(url);
       const room = createGame(roomName, userName);
 
       setHideMenu(true);
       // /#roomName[userName]
+      console.log(`url.includes(#)`);
     }
     // Detect hash entered on url bar
     window.onhashchange = () => {
       const urlEnteredManually = window.location.hash;
+      if (reg.test(urlEnteredManually)) {
+        alert(`YOU MAY PASS \n ${urlEnteredManually}`);
+      }
+      console.log(`TESTING REGEX =: ${reg.test(urlEnteredManually)}`);
       console.log(`hash ? ${urlEnteredManually}`);
 
       console.log(
@@ -45,6 +76,7 @@ const Index = () => {
       );
       setHideMenu(true);
     };
+    console.log(`url =: ${url}`);
   }, [url]);
 
   return (
