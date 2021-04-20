ab2cc7fe2cea0b0eb90441ec8d7a8bc179f74a13
Eder De Souza <esouza@student.42.fr>
2021-04-20 08:12:30 +0200

    Update controller games

--- server/src/controllers/games.ts
+++ server/src/controllers/games.ts
@@ -3,17 +3,7 @@ import GameSchema from '../schemas/GameSchema';
 import { Game } from '../models/Game';
 import { IGame } from '../interfaces/game.interface';
 
-export const getGames = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  // const game = new Game({ name: 'Shady' });
-
-  // console.log('game ?', game.name);
-  // console.log(game.fetch());
-
-  // res.status(200).json({ success: true, games: 'WIP' });
+export const getGames = async (req: Request, res: Response, next: NextFunction) => {
   GameSchema.find()
     .exec()
     .then((games) => {
@@ -31,21 +21,12 @@ export const createGame = async (
 ) => {
   const gameProps = req.body as IGame;
   const game = new Game(gameProps);
-
-  console.log(`createGame =: ${req.body.userName}`);
-  console.log(`GAME =: `, gameProps);
-
-  console.log(`gameName =: ${game.name}`);
-  console.log(`players =: `, game.players);
   game
     .save()
     .then((game) => {
       res.status(201).json({ success: true, game });
     })
     .catch((err) => {
-      console.log(
-        `after saving ERROR back to controller =: ${err} - ${err.message}`
-      );
-      res.status(400).json({ success: false, msg: err });
+      res.status(400).json({ success: false, msg: err.message });
     });
 };
