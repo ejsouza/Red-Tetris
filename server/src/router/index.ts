3e639fe41acaf8ced2e874945e4f0a92659dfea4
Eder De Souza <esouza@student.42.fr>
2021-04-24 08:48:14 +0200

    Create new route for game

--- server/src/router/index.ts
+++ server/src/router/index.ts
@@ -1,19 +1,39 @@
 import { Router } from 'express';
 import {
   getRooms,
-  getRoomsByName,
+  getRoomByName,
   createRoom,
-  deleteRoom,
+  deleteRoomByName,
+  deleteById,
 } from '../controllers/rooms/rooms';
+
+import { get, create, close, remove } from '../controllers/games';
 
 const router: Router = Router();
 
+/**
+ * Room
+ */
 router.get(`/rooms`, getRooms);
 
-router.get(`/rooms/:name`, getRoomsByName);
+router.get(`/rooms/:name`, getRoomByName);
 
 router.post(`/rooms`, createRoom);
 
-router.delete(`/rooms/:name`, deleteRoom);
+router.delete(`/rooms/:name`, deleteRoomByName);
+
+// router.delete(`/rooms/:id`, deleteById);
+
+/**
+ * Game
+ */
+
+router.get('/games', get);
+
+router.post('/games', create);
+
+router.put('/games', close);
+
+router.delete('/games', remove);
 
 export default router;
