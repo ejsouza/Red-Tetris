c995c75572ce32765c70fb631d0c2184b12c0af1
Eder De Souza <esouza@student.42.fr>
2021-04-29 08:13:07 +0200

    Update games controller

--- server/src/controllers/games/games.controller.ts
+++ server/src/controllers/games/games.controller.ts
@@ -1,4 +1,6 @@
 import * as express from 'express';
+import GameModelSchema from '../../schemas/GameSchema';
+import { Game } from '../../models/Game';
 import IGame from '../../interfaces/game.interface';
 
 export class GamesController {
@@ -16,10 +18,30 @@ export class GamesController {
 
   getAllGames = (req: express.Request, res: express.Response) => {
     // TODO
-    res.status(200).json({ success: true, msg: `Testing with classes` });
+    GameModelSchema.find()
+      .exec()
+      .then((games) => {
+        res.status(200).json({ success: true, games });
+      })
+      .catch((error) => {
+        res.status(404).json({ success: false, error });
+      });
   };
 
   createGame = (req: express.Request, res: express.Response) => {
     // TODO
+		const gameProps = req.body as IGame;
+    console.log(
+      `onCreate := ${gameProps.numberOfPlayers} - ${gameProps.name} - ${gameProps.players}`
+    );
+    const game = new Game(gameProps);
+    game
+      .save()
+      .then((game) => {
+        res.status(201).json({ success: true, game });
+      })
+      .catch((err) => {
+        res.status(400).json({ success: false, msg: err.message });
+      });
   };
 }
