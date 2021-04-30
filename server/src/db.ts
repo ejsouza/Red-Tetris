74be07818d7b5ee90741e6c2b7974713c3e0dea1
Eder De Souza <esouza@student.42.fr>
2021-04-30 07:53:07 +0200

    Rewrite db as a class

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,21 +1,21 @@
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
 dotenv.config();
-const {
-	MONGO_USERNAME,
-	MONGO_PASSWORD,
-	MONGO_HOSTNAME,
-	MONGO_CONTAINER,
-	MONGO_PORT,
-	MONGO_DB,
-} = process.env;
+// const {
+// 	MONGO_USERNAME,
+// 	MONGO_PASSWORD,
+// 	MONGO_HOSTNAME,
+// 	MONGO_CONTAINER,
+// 	MONGO_PORT,
+// 	MONGO_DB,
+// } = process.env;
 
-const options = {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false,
-};
+// const options = {
+// 	useNewUrlParser: true,
+// 	useCreateIndex: true,
+// 	useUnifiedTopology: true,
+// 	useFindAndModify: false,
+// };
 
 // const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
@@ -24,14 +24,36 @@ const options = {
 // }).catch((error: any) => {
 // 	console.log(`MangoDB ERROR >>> ${error}`)
 // });
-console.log('here ', MONGO_PORT)
-const urlLocal = `mongodb://localhost:${MONGO_PORT}/${MONGO_DB}`
-const urlDocker = `mongodb://${MONGO_USERNAME}:${MONGO_PORT}/${MONGO_DB}`
+// console.log('here ', MONGO_PORT)
+// const urlLocal = `mongodb://localhost:${MONGO_PORT}/${MONGO_DB}`
+// const urlDocker = `mongodb://${MONGO_USERNAME}:${MONGO_PORT}/${MONGO_DB}`
 
-const connectDB = async () => {
-	const conn = await mongoose.connect(urlLocal, options)
-	console.log(`MongoDB connected: ${conn.connection.host}`);
-	return conn;
+// const connectDB = async () => {
+// 	const conn = await mongoose.connect(urlLocal, options)
+// 	console.log(`🏠 MongoDB connected: ${conn.connection.host}`);
+// 	return conn;
+// }
+
+// export default connectDB;
+
+export class DB {
+  private urlLocal = `mongodb://localhost:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`;
+  private urlDocker = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`;
+  private options = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  };
+  constructor() {
+    this.connectToDB();
+  }
+
+  private async connectToDB(): Promise<void> {
+    mongoose.connect(this.urlLocal, this.options).then((conn) => {
+      console.log(` 🏠 MongoDB connected on ${conn.connection.host}`);
+      console.log(` 💾 Connection to database successfull`);
+      console.log(' 🛑 [stop] Press CTRL-C\n');
+    });
+  }
 }
-
-export default connectDB;
