c3b3fd7f9098c707325b83220ece48e734cfab6d
Eder De Souza <esouza@student.42.fr>
2021-05-07 07:56:00 +0200

    Update parse url function

--- client/utils/parseUrlWithHash.ts
+++ client/utils/parseUrlWithHash.ts
@@ -1,14 +1,13 @@
 const parseUrlWithHash = (url: string): string[] => {
-	const path = url.startsWith('/') ? url.slice(1, url.length) : url;
-  const startSeparator = path.indexOf('[');
-  const endSeparator = path.indexOf(']');
-  /**
-   * When url entered through the form the url starts with '/'
-   * When url entered directly on the browser url starts with '#'
-   */
-  const roomName = path.slice(1, startSeparator);
-  const userName = path.slice(startSeparator + 1, endSeparator);
-  return [roomName, userName];
+
+  console.log(`In parseUrlWithHash (${url})`);
+  if (!url.includes('#')) {
+    return [];
+  }
+  const game = url.match(/\/#([a-z]+)/gi)![0].slice(2);
+  const player = url.match(/\[([a-z]+)/gi)![0].slice(1);
+
+  return [game, player];
 };
 
 export default parseUrlWithHash;
