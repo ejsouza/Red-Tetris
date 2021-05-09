0feed627d50521215516a1ececa7cc9f3c5bab1b
Eder De Souza <esouza@student.42.fr>
2021-05-09 12:18:25 +0200

    Update games controller

--- server/src/controllers/games/games.controller.ts
+++ server/src/controllers/games/games.controller.ts
@@ -1,25 +1,28 @@
 import * as express from 'express';
 import GameModelSchema from '../../schemas/GameSchema';
 import { Game } from '../../models/Game';
-import IGame from '../../interfaces/game.interface';
+import { IGame } from '../../interfaces/game.interface';
+
 
 export class GamesController {
-  public path = '/games';
-  public router = express.Router();
+  private _path = '/games';
+  private _router = express.Router();
+  private _game = GameModelSchema;
 
   constructor() {
     this.initializeRoutes();
   }
 
   public initializeRoutes() {
-    this.router.get(this.path, this.getAllGames);
-    this.router.post(this.path, this.createGame);
+    this._router.get(this._path, this.getAllGames);
+    this._router.get(`${this._path}/:id`, this.getByName);
+    this._router.post(this._path, this.createGame);
   }
 
   getAllGames = (req: express.Request, res: express.Response) => {
     // TODO
-    GameModelSchema.find()
-      .exec()
+    this._game
+      .find()
       .then((games) => {
         res.status(200).json({ success: true, games });
       })
@@ -28,19 +31,35 @@ export class GamesController {
       });
   };
 
+  getByName = (req: express.Request,  res: express.Response) => {
+    const {  id  } = req.params;
+
+    this._game
+      .findOne({  name: id  })
+        .then((game) => {
+          if (!game) {
+            return res.status(200).json({ success: false, ok: false, game });
+          }
+          res.status(200).json({ success: true, ok: true, game });;
+        })
+        .catch((err) => {
+          res.status(500).json({ success: false, err });;
+        });
+  };
+
   createGame = (req: express.Request, res: express.Response) => {
     // TODO
-		const gameProps = req.body as IGame;
+    const gameProps = req.body as IGame;
     console.log(
       `onCreate := ${gameProps.numberOfPlayers} - ${gameProps.name} - ${gameProps.players}`
     );
-    const game = new Game(gameProps);
-    game
+    const newGame = new this._game(gameProps);
+    newGame
       .save()
-      .then((game) => {
+      .then((game: IGame) => {
         res.status(201).json({ success: true, game });
       })
-      .catch((err) => {
+      .catch((err: Error) => {
         res.status(400).json({ success: false, msg: err.message });
       });
   };
