25ca984352dffd82d85cf3f0548ff42f99a6275e
Eder De Souza <esouza@student.42.fr>
2021-05-10 19:00:50 +0200

    Update index

--- rtAPI/src/index.ts
+++ rtAPI/src/index.ts
@@ -1,3 +1,6 @@
 import { Socket } from '../src/models/Socket';
+import { Server } from '../src/models/Server';
 let games: { game: string; players: string[] }[] = [];
 new Socket(games);
+
+// new Server();
