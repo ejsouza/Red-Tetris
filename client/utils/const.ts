0ca888ebc0fd705aed7b19ff252abc711d5e2a64
Eder De Souza <esouza@student.42.fr>
2021-05-12 15:12:23 +0200

    Update constants

--- client/utils/const.ts
+++ client/utils/const.ts
@@ -1,3 +1,6 @@
 export const APIurl: string = 'http://localhost:8000/api/v1';
 export const BASEurl: string = 'http://localhost:8000';
 export const RT_API: string = 'http://localhost:5000';
+
+export const BOARD_HEIGHT = 20;
+export const BOARD_WIDTH = 10;
