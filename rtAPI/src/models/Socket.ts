97e0bb74cbd33f8f7b03dfaed6b6029d18025d00
Eder De Souza <esouza@student.42.fr>
2021-05-15 16:20:16 +0200

    Update Socket model

--- rtAPI/src/models/Socket.ts
+++ rtAPI/src/models/Socket.ts
@@ -2,8 +2,23 @@ import * as htpp from 'http';
 import * as socketIO from 'socket.io';
 import * as dotenv from 'dotenv';
 import { Game } from './Game';
+import {
+  updateBoard,
+  movePiece,
+  startGameInterval,
+  createBoard,
+  removeOldPiece,
+  draw,
+} from '../utils/game';
 
 dotenv.config();
+
+interface Piece {
+  pos: { x: number; y: number }[];
+  width: number;
+  height: number;
+  color: number;
+}
 
 export class Socket {
   private _httpServer: htpp.Server;
@@ -11,6 +26,8 @@ export class Socket {
   private _PORT: number;
   private _CLIENT_URL: string;
   private _data: { game: string; players: string[] }[];
+  private _board: number[][];
+  private _piece: Piece;
 
   constructor(games: { game: string; players: string[] }[]) {
     this._data = games;
@@ -34,7 +51,6 @@ export class Socket {
     });
 
     this._io.on('connection', (socket: socketIO.Socket) => {
-      console.log(`SOCKETIO running... ${socket}`);
 
       socket.on('join', (arg) => {
         console.log(`got from client := ${arg.name} <--> ${arg.game}`);
@@ -42,26 +58,63 @@ export class Socket {
 
       socket.on('start', () => {
         this._io.emit('closeStartComponent');
-        const  game = new  Game(this._io, socket);
-        game.start();
-        game.on();
+        this._io.emit('setGame', { start: true });
+        // [this._board, this._piece] = buildNewMap();
+     
+        [this._board, this._piece] = createBoard(socket);
+        startGameInterval(socket, this._board, this._piece);
+
       });
-      socket.on('joinDetails', (arg: { name: string; room: string }) => {
-        const simple = {
-          game: arg.room,
-          players: [arg.name],
-        };
-        console.log(`new user ${arg.name} in  room ${arg.room}`);
-        this._data.push(simple);
-        console.log(`added? ${this._data}`);
-        console.log(`obje len := ${this._data.length}`);
-        for (let d of this._data) {
-          console.log(
-            `d := ${d} -- d.game := ${d.game} -- d.players := ${d.players}`
-          );
-        }
-        socket.emit('joinDetails', { success: true });
-      });
+
+      socket.on('updateMove', () => console.log(`update this`));
+
+
+      // socket.on('getGameMap', () => {
+      //   [this._board, this._piece] = buildNewMap();
+      //   this._io.emit('newMap', this._board, this._piece);
+      // });
+
+      // socket.on('updateMap', (board: number[][], piece: Piece, delay: Date) => {
+      //   [this._board, this._piece] = updateBoard(board, piece);
+      //   setTimeout(() => {
+      //     this._io.emit('mapUpdated', this._board, this._piece);
+      //     console.log(`PIECE.X := ${piece.x} PIECE.Y := ${piece.y} -- ${Date.now() - delay}`);
+      //   }, (1000 * 60 ) / 60, );
+      // });
+
+      // // Listen for move piece
+      // socket.on(
+      //   'move',
+      //   (board: number[][], piece: Piece, direction: number) => {
+      //     [this._board, this._piece] = movePiece(
+      //       board,
+      //       piece,
+      //       direction
+      //     );
+      //     process.nextTick(() => {
+      //       this._io.emit('updateMove', this._board, this._piece);
+      //     });
+      //   }
+      // );
+
+      // socket.on('joinDetails', (arg: { name: string; room: string }) => {
+      //   const simple = {
+      //     game: arg.room,
+      //     players: [arg.name],
+      //   };
+      //   console.log(`new user ${arg.name} in  room ${arg.room}`);
+      //   this._data.push(simple);
+      //   console.log(`added? ${this._data}`);
+      //   console.log(`obje len := ${this._data.length}`);
+      //   for (let d of this._data) {
+      //     console.log(
+      //       `d := ${d} -- d.game := ${d.game} -- d.players := ${d.players}`
+      //     );
+      //   }
+      //   socket.emit('joinDetails', { success: true });
+      // });
+
+      return;
     });
   }
 
