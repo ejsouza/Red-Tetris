b5fe13a7903b04c5211f67546aaedc0e54e7409c
Eder De Souza <esouza@student.42.fr>
2021-05-16 08:13:08 +0200

    Create mothods on the Front for handling game actions

---
+++ client/core/gameEngine.ts
@@ -0,0 +1,144 @@
+import { BOARD_HEIGHT, BOARD_WIDTH } from '../utils/const';
+
+interface Piece {
+  pos: { x: number; y: number }[];
+  width: number;
+  height: number;
+  color: number;
+}
+
+const cleanPieceFromBoard = (board: number[][], piece: Piece): void => {
+  piece.pos.forEach((pos) => {
+    board[pos.x][pos.y] = 0;
+  });
+};
+
+const isYLessThanZero = (piece: Piece): boolean => {
+  let isLess = false;
+  piece.pos.forEach((pos) => {
+    if (pos.y - 1 < 0) {
+      isLess = true;
+    }
+  });
+  return isLess;
+};
+
+const isYGreaterThanWidth = (piece: Piece): boolean => {
+  let isGreater = false;
+  piece.pos.forEach((pos) => {
+    if (pos.y > BOARD_WIDTH - piece.width) {
+      isGreater = true;
+    }
+  });
+  return isGreater;
+};
+
+const isXPlusOneFree = (board: number[][], piece: Piece): boolean => {
+  let isFree = false;
+  let lastX = 0;
+  let lastY = 0;
+
+  piece.pos.forEach((pos) => {
+    lastX = pos.x;
+    lastY = pos.y;
+    if (pos.x + 1 < BOARD_HEIGHT) {
+      isFree = true;
+    } else {
+      isFree = false;
+    }
+  });
+  console.log(`lastX := ${lastX}`);
+  if (
+    (lastX < BOARD_HEIGHT - 1 && board[lastX + 1][lastY - 1] !== 0) ||
+    board[lastX + 1][lastY] !== 0
+  ) {
+    isFree = false;
+  }
+  return isFree;
+};
+
+const updateHorizontalPosition = (piece: Piece, value: number): void => {
+  piece.pos.forEach((pos) => {
+    pos.y = pos.y + value;
+  });
+};
+
+const updateVerticalPosition = (piece: Piece): void => {
+  piece.pos.forEach((pos) => {
+    pos.x = pos.x + 1;
+  });
+};
+
+const writeNewPieceToBoard = (board: number[][], piece: Piece): void => {
+  piece.pos.forEach((pos) => {
+    board[pos.x][pos.y] = piece.color;
+  });
+};
+
+export const updateBoard = (
+  board: number[][],
+  piece: Piece,
+  key: number
+): void => {
+  switch (key) {
+    case 37: {
+      if (!isYLessThanZero(piece)) {
+        cleanPieceFromBoard(board, piece);
+        updateHorizontalPosition(piece, -1);
+        writeNewPieceToBoard(board, piece);
+      }
+      break;
+    }
+    case 39: {
+      if (!isYGreaterThanWidth(piece)) {
+        cleanPieceFromBoard(board, piece);
+        updateHorizontalPosition(piece, 1);
+        writeNewPieceToBoard(board, piece);
+      }
+      break;
+    }
+    case 40: {
+      if (isXPlusOneFree(board, piece)) {
+        cleanPieceFromBoard(board, piece);
+        updateVerticalPosition(piece);
+        writeNewPieceToBoard(board, piece);
+      }
+      break;
+    }
+
+    default:
+      break;
+  }
+};
+
+export const gameLoo = (
+  board: number[][],
+  piece: Piece
+): [board: number[][], piece: Piece] => {
+  const ROWS = 20;
+  // const stopGame = (id: number) => clearInterval(id);
+
+  // const loop = (board: number[][], piece: Piece): void => {
+  //   console.log('[setInterval board] ', board);
+  // };
+
+  // const stopGameId = setInterval(loop, 1000, board, piece);
+  console.log('getting here? ', piece.x);
+  if (piece.x < ROWS - piece.height) {
+    for (let y = 0, by = piece.y; y < piece.tetromino.length; y++, by++) {
+      for (let x = 0, bx = piece.x; x < piece.tetromino.length; x++, bx++) {
+        if (board[bx][by] === piece.tetromino[x][y]) {
+          board[bx][by] = 0;
+        }
+      }
+    }
+    piece.x += 1;
+    console.log(`enters condition ? ${piece.x}`);
+    for (let y = 0, by = piece.y; y < piece.tetromino.length; y++, by++) {
+      for (let x = 0, bx = piece.x; x < piece.tetromino.length; x++, bx++) {
+        board[bx][by] = piece.tetromino[x][y];
+      }
+    }
+  }
+  return [board, piece];
+};
