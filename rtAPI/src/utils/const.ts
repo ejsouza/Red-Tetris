9f699850945ab2f08773211313b407b5ee19fb0d
Eder De Souza <esouza@student.42.fr>
2021-05-18 07:11:24 +0200

    Create new pieces

--- rtAPI/src/utils/const.ts
+++ rtAPI/src/utils/const.ts
@@ -1,5 +1,7 @@
 export const COLUMNS = 10;
 export const ROWS = 20;
+export const BOARD_HEIGHT = 20;
+export const BOARD_WIDTH = 10;
 
 export const TETRIS = [
   {
@@ -104,5 +106,27 @@ export const PIECES = [
     width: 2,
     height: 2,
     color: 2,
+  },
+  {
+    pos: [
+      { x: 0, y: 3 },
+      { x: 1, y: 3 },
+      { x: 1, y: 4 },
+      { x: 1, y: 5 },
+    ],
+    width: 2,
+    height: 2,
+    color: 3,
+  },
+  {
+    pos: [
+      { x: 0, y: 5 },
+      { x: 1, y: 5 },
+      { x: 1, y: 4 },
+      { x: 1, y: 3 },
+    ],
+    width: 2,
+    height: 2,
+    color: 4,
   },
 ];
