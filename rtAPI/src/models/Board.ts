1c6105a3f98227b11481a0e56b90b5e75e52f7de
Eder De Souza <esouza@student.42.fr>
2021-05-21 07:16:57 +0200

    Create Board model

---
+++ rtAPI/src/models/Board.ts
@@ -0,0 +1,24 @@
+import { BOARD_WIDTH, BOARD_HEIGHT } from '../utils/const';
+import { IPiece } from '../interfaces/piece.interface';
+
+export class Board {
+  public shape: number[][];
+
+  constructor() {
+    this.shape = Array.from({ length: BOARD_HEIGHT }, () =>
+      Array(BOARD_WIDTH).fill(0)
+    );
+  }
+
+  drawPiece(piece: IPiece) {
+    piece.pos.forEach((pos) => {
+      this.shape[pos.x][pos.y] = piece.color;
+    });
+  }
+
+  cleanPiece(piece: IPiece) {
+    piece.pos.forEach((pos) => {
+      this.shape[pos.x][pos.y] = 0;
+    });
+  }
+}
