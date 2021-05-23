c4f2f94081efa27e4a7006d87e28ed2dc447bbcb
Eder De Souza <esouza@student.42.fr>
2021-05-23 06:55:07 +0200

    Update client gameEngine

--- client/core/gameEngine.ts
+++ client/core/gameEngine.ts
@@ -9,16 +9,16 @@ interface Piece {
 
 const cleanPieceFromBoard = (board: number[][], piece: Piece): void => {
   piece.pos.forEach((pos) => {
-    board[pos.x][pos.y] = 0;
+    board[pos.y][pos.x] = 0;
   });
 };
 
 const isYLessThanZero = (piece: Piece): boolean => {
   let isLess = false;
   piece.pos.forEach((pos) => {
-    if (pos.y - 1 < 0) {
+    if (pos.x - 1 < 0) {
       isLess = true;
-			return;
+      return;
     }
   });
   return isLess;
@@ -27,9 +27,9 @@ const isYLessThanZero = (piece: Piece): boolean => {
 const isYGreaterThanWidth = (piece: Piece): boolean => {
   let isGreater = false;
   piece.pos.forEach((pos) => {
-    if (pos.y + 1 > BOARD_WIDTH) {
+    if (pos.x + 1 > BOARD_WIDTH) {
       isGreater = true;
-			return;
+      return;
     }
   });
   return isGreater;
@@ -38,19 +38,19 @@ const isYGreaterThanWidth = (piece: Piece): boolean => {
 
 const updateHorizontalPosition = (piece: Piece, value: number): void => {
   piece.pos.forEach((pos) => {
-    pos.y = pos.y + value;
+    pos.x = pos.x + value;
   });
 };
 
 const updateVerticalPosition = (piece: Piece): void => {
   piece.pos.forEach((pos) => {
-    pos.x = pos.x + 1;
+    pos.y = pos.y + 1;
   });
 };
 
 const writeNewPieceToBoard = (board: number[][], piece: Piece): void => {
   piece.pos.forEach((pos) => {
-    board[pos.x][pos.y] = piece.color;
+    board[pos.y][pos.x] = piece.color;
   });
 };
 
@@ -59,11 +59,11 @@ const leftCellIsFree = (board: number[][], piece: Piece): boolean => {
 
 	cleanPieceFromBoard(board,  piece);
 	piece.pos.forEach(pos => {
-		if (pos.y - 1 < 0 || board[pos.x][pos.y - 1] !== 0) {
-			isFree = false;
-			writeNewPieceToBoard(board, piece);
-			return;
-		}
+		if (pos.x - 1 < 0 || board[pos.y][pos.x - 1] !== 0) {
+      isFree = false;
+      writeNewPieceToBoard(board, piece);
+      return;
+    }
 	})
   return isFree;
 }
@@ -73,7 +73,7 @@ const rightCellIsFree = (board: number[][], piece: Piece): boolean => {
 
   cleanPieceFromBoard(board, piece);
   piece.pos.forEach((pos) => {
-    if (pos.y + 1 > BOARD_WIDTH || board[pos.x][pos.y + 1] !== 0) {
+    if (pos.x + 1 > BOARD_WIDTH || board[pos.y][pos.x + 1] !== 0) {
       isFree = false;
       writeNewPieceToBoard(board, piece);
       return;
@@ -87,7 +87,7 @@ const isXPlusOneFree = (board: number[][], piece: Piece): boolean => {
 
    cleanPieceFromBoard(board, piece);
    piece.pos.forEach((pos) => {
-     if (pos.x + 1 >= BOARD_HEIGHT || ( board[pos.x + 1][pos.y] !== 0)) {
+     if (pos.y + 1 >= BOARD_HEIGHT || board[pos.y + 1][pos.x] !== 0) {
        isFree = false;
        writeNewPieceToBoard(board, piece);
        return;
@@ -95,6 +95,29 @@ const isXPlusOneFree = (board: number[][], piece: Piece): boolean => {
    });
    return isFree;
 }
+
+const rotate = (board: number[][], piece: Piece): void => {
+  const diff = Math.floor(piece.height / 2);
+  if (piece.pos[0].y - diff < 0) {
+    return;
+  }
+  console.log(`before rotating ${piece.pos[0].y}`);
+  piece.pos.forEach((pos) => console.log(`[${pos.y}][${pos.x}]`));
+  // cleanPieceFromBoard(board, piece);
+  // piece.pos.forEach((pos) => {
+  //   const tmpY = pos.y;
+  //   pos.y = pos.x - diff;
+  //   pos.x = tmpY;
+  // });
+  // writeNewPieceToBoard(board, piece);
+  console.log(`after rotating ${piece.pos[0].y}`);
+
+  for (let y = 0; y < piece.height; y++) {
+    for (let x = 0; x < piece.width; x++) {
+      console.log(`${piece.pos[x].y} - ${piece.pos[x].x}`);
+    }
+  }
+};
 
 export const updateBoard = (
   board: number[][],
@@ -112,6 +135,7 @@ export const updateBoard = (
     }
 		case 38: {
 			console.log(`Turn piece around`);
+			rotate(board, piece);
 			break;	
 		}
     case 39: {
