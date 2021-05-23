a4b509c521f137da7fd45a68cb7e723d63fb37e9
Eder De Souza <esouza@student.42.fr>
2021-05-23 06:56:27 +0200

    Update Piece Model

--- rtAPI/src/models/Piece.ts
+++ rtAPI/src/models/Piece.ts
@@ -3,8 +3,22 @@ import { IPiece } from '../interfaces/piece.interface';
 
 export class Piece {
   public shape: IPiece;
+  private _pieces: number[];
 
   constructor() {
-    this.shape = PIECES[Math.floor(Math.random() * 7)];
+    // this.shape = PIECES[Math.floor(Math.random() * 7)];
+		this._pieces = [];
+    // this.shape = this.randomPiece();
   }
+  randomPiece = (): IPiece => {
+    if (this._pieces.length === 0) {
+      this._pieces = [0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6];
+    }
+		return PIECES[
+      this._pieces.splice(
+        Math.floor(Math.random() * this._pieces.length),
+        1
+      )[0]
+    ];
+  };
 }
