17bc9705696d9733f8e92d8f2c7b761550a65572
Eder De Souza <esouza@student.42.fr>
2021-05-24 08:41:07 +0200

    Update Front constants

--- client/utils/const.ts
+++ client/utils/const.ts
@@ -4,3 +4,29 @@ export const RT_API: string = 'http://localhost:5000';
 
 export const BOARD_HEIGHT = 20;
 export const BOARD_WIDTH = 10;
+
+
+export const EMPTY_PIECE = [
+  {
+    pos: [
+      { x: 0, y: 0 },
+      { x: 0, y: 0 },
+      { x: 0, y: 0 },
+      { x: 0, y: 0 },
+    ],
+    width: 0,
+    height: 0,
+    color: 0,
+  },
+];
+
+export interface IPiece {
+  pos: { x: number; y: number }[];
+  width: number;
+  height: number;
+  color: number;
+}
+
+export interface IBoard {
+  board: number[][];
+}
