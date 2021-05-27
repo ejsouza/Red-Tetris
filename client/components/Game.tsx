9aace45251efbb017215cdea18d0dc4a3027e87e
Eder De Souza <esouza@student.42.fr>
2021-05-27 07:18:29 +0200

    Update Game component

--- client/components/Game.tsx
+++ client/components/Game.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState, KeyboardEventHandler } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import BoardGame from '../components/BoardGame';
 import styled, { createGlobalStyle, css } from 'styled-components';
 import socket from '../utils/socket';
 import Loading from '../components/Loading';
 import {BOARD_HEIGHT, BOARD_WIDTH} from '../utils/const';
-import {updateBoard} from '../core/gameEngine';
+import { updateBoard, cleanPieceFromBoard } from '../core/gameEngine';
 
 const Container = styled.div`
   padding: 16px;
@@ -24,11 +24,76 @@ interface Piece {
   color: number;
 }
 
+interface ICallback {
+  fn: () => void
+}
 
 const Game = () => {
   const [map, setMap] = useState<number[][]>();
   const [newMap, setNewMap] = useState(false);
   const [piece, setPiece] = useState<Piece>();
+  const mapRef = useRef(map);
+
+  // ---- TEST LOOP ON THE FRONT ----
+
+  
+function useInterval(callback: ICallback, delay: number) {
+  const savedCallback = useRef<ICallback | null>(null);
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+  const updateGame = () => {
+    console.log(`updateGame() called`);
+    if (!map || !piece) {
+      return;
+    }
+    console.log('gets here');
+    cleanPieceFromBoard(map, piece);
+    setMap([...map]);
+  };
+  const gameLoop = (board: number[][], piece: Piece, nextPiece: Piece) => {
+    console.log(`gameloop `, map);
+    let count = 0;
+    const interval = setInterval(
+      () => {
+        count++;
+        updateGame();
+        console.log(`map ${map}`);
+        console.log(`counting ${count}`);
+        if (count === 5) {
+          clearInterval(interval);
+        }
+      },
+      (900 * 60) / 100,
+      map,
+      piece
+    );
+  };
+  useEffect(() => {
+    socket.on('newMap', (board: number[][], piece: Piece, nextPiece: Piece) => {
+      console.log('got newMap ', board);
+      setMap(board);
+      setPiece(piece);
+      gameLoop(board , piece, nextPiece);
+    });
+  }, []);
+
+  // ---- ENT TESTING
 
   useEffect(() => {
     socket.on('gameState', (board: number[][], piece: Piece) => {
@@ -39,7 +104,6 @@ const Game = () => {
 
   useEffect(() => {
     socket.on('newMap', (map: number[][], piece: Piece) => {
-      console.log('got newMap on front');
       setMap(map);
       setPiece(piece);
     });
@@ -52,7 +116,6 @@ const Game = () => {
 
   const handleKeyDown = (e: any) => {
     if (!piece || !map) {
-      console.log(`returning....`);
       return;
     }
 
@@ -64,12 +127,12 @@ const Game = () => {
     }
     if (e.key === 'ArrowDown') {
       updateBoard(map, piece, e.keyCode);
+      setMap(map);
     }
     if (e.key === 'ArrowUp') {
-      console.log('UP UP UP ', e.keyCode);
       updateBoard(map, piece, e.keyCode);
     }
-   
+
     socket.emit('keydown', { key: e.keyCode, board: map, piece: piece });
   };
 
