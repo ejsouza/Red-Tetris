58e6b094a386a87d332c5e1032e3817428217d38
Eder De Souza <esouza@student.42.fr>
2021-05-29 08:58:45 +0200

    Update server games controller

--- server/src/controllers/games/games.controller.ts
+++ server/src/controllers/games/games.controller.ts
@@ -5,8 +5,8 @@ import { IGame } from '../../interfaces/game.interface';
 
 
 export class GamesController {
-  private _path = '/games';
-  private _router = express.Router();
+  public path = '/games';
+  public router = express.Router();
   private _game = GameModelSchema;
 
   constructor() {
@@ -14,9 +14,10 @@ export class GamesController {
   }
 
   public initializeRoutes() {
-    this._router.get(this._path, this.getAllGames);
-    this._router.get(`${this._path}/:id`, this.getByName);
-    this._router.post(this._path, this.createGame);
+    this.router.get(this.path, this.getAllGames);
+    this.router.get(`${this.path}/:id`, this.getByName);
+    this.router.post(this.path, this.createGame);
+    this.router.patch(`${this.path}/:id`, this.addPlayerToGame );
   }
 
   getAllGames = (req: express.Request, res: express.Response) => {
@@ -31,20 +32,20 @@ export class GamesController {
       });
   };
 
-  getByName = (req: express.Request,  res: express.Response) => {
-    const {  id  } = req.params;
+  getByName = (req: express.Request, res: express.Response) => {
+    const { id } = req.params;
 
     this._game
-      .findOne({  name: id  })
-        .then((game) => {
-          if (!game) {
-            return res.status(200).json({ success: false, ok: false, game });
-          }
-          res.status(200).json({ success: true, ok: true, game });;
-        })
-        .catch((err) => {
-          res.status(500).json({ success: false, err });;
-        });
+      .findOne({ name: id })
+      .then((game) => {
+        if (!game) {
+          return res.status(200).json({ success: false, ok: false, game });
+        }
+        res.status(200).json({ success: true, ok: true, game });
+      })
+      .catch((err) => {
+        res.status(500).json({ success: false, err });
+      });
   };
 
   createGame = (req: express.Request, res: express.Response) => {
@@ -63,4 +64,23 @@ export class GamesController {
         res.status(400).json({ success: false, msg: err.message });
       });
   };
+
+  addPlayerToGame = (req: express.Request, res: express.Response) => {
+    const props = req.body as IGame;
+    const {name, players} = props;
+    console.log(`game ${name} players ${players}`);
+    const filter = {name: name};
+    const update = { $push: { players: players }, $inc: {'numberOfPlayers': 1}};
+    this._game
+      .findOneAndUpdate(filter, update, { new: true })
+      .then((game) => {
+         if (!game) {
+          return res.status(200).json({ success: false, ok: false, game });
+        }
+        res.status(200).json({ success: true, ok: true, game });
+      })
+      .catch((err) => {
+        res.status(500).json({ success: false, err });
+      });
+  }
 }
