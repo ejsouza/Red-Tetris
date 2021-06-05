1bf3886332832b4c517fd8b668f976b01afbd508
Eder De Souza <esouza@student.42.fr>
2021-06-05 07:00:57 +0200

    Update Piece model

--- rtAPI/src/models/Piece.ts
+++ rtAPI/src/models/Piece.ts
@@ -14,37 +14,15 @@ export class Piece {
     if (this._pieces.length === 0) {
       // console.log('Create a new random full piece');
       this._pieces = [
-        0,
-        0,
-        0,
-        0,
-        1,
-        1,
-        1,
-        1,
-        2,
-        2,
-        2,
-        2,
-        3,
-        3,
-        3,
-        3,
-        4,
-        4,
-        4,
-        4,
-        5,
-        5,
-        5,
-        5,
-        6,
-        6,
-        6,
-        6,
+        0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5,
+        6, 6, 6, 6,
       ];
     }
-    // console.log('new piece requested\n');
+    // console.log(
+    //   `new piece requested - RANDOM ${Math.floor(
+    //     Math.random() * this._pieces.length
+    //   )} LEN ${this._pieces.length} \n`
+    // );
     return PIECES[
       this._pieces.splice(Math.floor(Math.random() * this._pieces.length), 1)[0]
     ];
