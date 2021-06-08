190d4db9ebfc5e7f03e65221ac373b7ba842461e
Eder De Souza <esouza@student.42.fr>
2021-06-08 07:05:32 +0200

    Upate boardComponent

--- client/components/BoardGame.tsx
+++ client/components/BoardGame.tsx
@@ -5,9 +5,12 @@ const sizeOfGrid = 10;
 
 const Grid = styled.div`
   display: grid;
-  max-width: 360px;
-  grid-template-columns: repeat(${sizeOfGrid}, auto);
-  // grid-template-rows: repeat(20, auto);
+  // width: 100%;
+  // max-width: 360px;
+  // max-height: 800vh;
+  grid-template-columns: repeat(${sizeOfGrid}, 1fr);
+  // grid-template-columns: repeat(${sizeOfGrid}, minmax(10px, 1fr));
+  // grid-template-rows: repeat(20, 1fr);
   // grid-template-columns: repeat(${sizeOfGrid}, calc(360px / ${sizeOfGrid}));
   // grid-template-row: repeat(${sizeOfGrid}, calc(360px / ${sizeOfGrid}));
 
@@ -17,13 +20,16 @@ const Grid = styled.div`
   background-color: #2196f3;
   border-bottom: 1px solid rgba(255, 255, 255, 1);
   border-left: 1px solid rgba(255, 255, 255, 1);
+  // gap 1px 1px;
   line-height: 1;
 `;
 
 const GridItem = styled.div`
   background-color: rgba(255, 255, 255, 0.8);
+  // max-height: 2vh;
+  // max-width: 2vw;
   // background-color: #596269;
-  box-sizing: border-box;
+  // box-sizing: border-box;
   border-top: 1px solid rgba(255, 255, 255, 1);
   border-right: 1px solid rgba(255, 255, 255, 1);
   padding: 10px;
