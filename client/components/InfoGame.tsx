352d49c01494789a920f120934224f79ca37955d
Eder De Souza <esouza@student.42.fr>
2021-06-10 08:28:50 +0200

    Update infoGame component

--- client/components/InfoGame.tsx
+++ client/components/InfoGame.tsx
@@ -18,8 +18,34 @@ const Wrapper = styled.div`
   border: 1px solid rgba(255, 255, 255, 1);
 `;
 
+const sizeOfGrid = 10;
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(${sizeOfGrid}, 1fr);
+  background-color: #2196f3;
+  border-bottom: 1px solid rgba(255, 255, 255, 1);
+  border-left: 1px solid rgba(255, 255, 255, 1);
+  line-height: 1;
+  max-height: 270px;
+  max-width: 130px;
+`;
+
+const GridItem = styled.div`
+  background-color: rgba(255, 255, 255, 0.8);
+  max-height: 5px;
+  max-width: 5px;
+  border-top: 1px solid rgba(255, 255, 255, 1);
+  border-right: 1px solid rgba(255, 255, 255, 1);
+  padding: 6px;
+  span {
+    opacity: 0;
+  }
+`;
+
+
 interface IProps {
   player: string;
+	game: string;
   piece: IPiece;
   board: number[][];
 }
@@ -46,35 +72,135 @@ interface IShadow {
 }
 
 const Parent = styled.div`
-  display: block;
-  // width: 200px;
+  max-width: 308px;
 `;
 
 export const InfoGame = (props: IProps) => {
   const piece: number[][] = Array.from({ length: 2 }, () => Array(10).fill(0));
   const [boardShadow, setBoardShadow] = useState<number[][]>();
-	// const [shadows, setShadow] = useState<IShadow[]>([])
+  // const [shadows, setShadow] = useState<IShadow[]>([])
+  const [shadows, setShadows] = useState<IShadow[]>([]);
+	const [loading, setLoading] = useState(false);
+	const [player, setPlayer] = useState<IShadow>();
+	// const [shades, setShades] = useState<JSX.Element[][]>([]);
+	const [shades, setShades] = useState<JSX.Element[][][]>([]);
+	const [toggle, setToggle] = useState(false);
 
   props.piece.pos.forEach((pos) => {
     piece[pos.y][pos.x] = props.piece.color;
   });
 
+ useEffect(() => {
+   setTimeout(() => {
+     setLoading(true);
+   }, 2000);
+ }, []);
+
+  useEffect(() => {
+    socket.on('boardShadow', (board: number[][]) => {
+      setBoardShadow(board);
+    });
+  }, []);
+
+  // useEffect(() => {
+  //   socket.on('shaddy', (player: IShadow) => {
+	// 		setPlayer(player);
+
+	// 		setShadows(
+	// 			shadows.map(shadow => shadow.player === player.player ? {...shadow} : player)
+	// 		)
+  //   });
+  // }, []);
+
 	useEffect(() => {
- 		socket.on('boardShadow', (board: number[][]) => {
-   		setBoardShadow(board);
- 		});
-	})
- 
+		socket.on('shaddy', () => {
+			setToggle(!toggle);
+		})
+	}, []);
 
 
-	// useEffect(() => {
+	useEffect(() => {
+    // When page first load get array of players with their board
+    socket.emit('getArrayOfPlayers', props.game, props.player);
+  }, [player, toggle]);
+
+	useEffect(() => {
+    socket.on('arrayOfPlayers', (shadows: IShadow[]) => {
+      setShadows([...shadows]);
+    });
+  }, []);
+
+		useEffect(() => {
+      let index = 0;
+
+      const colors = [
+        '',
+        '#ECF00B',
+        '#8C00EC',
+        '#1100EC',
+        '#EB8E08',
+        '#45F304',
+        '#E90005',
+        '#48EFEC',
+        '#FC03db',
+      ];
+
+      //   if(shadows) {
+      //     let board: JSX.Element[][] = [];
+
+      //     shadows.forEach((shade) => {
+      //       board = Object.entries(shade.board).map((row) => {
+      //         return row[1].map((cell) => (
+      //           <GridItem
+      //             style={{ background: colors[cell] }}
+      //             key={`cell-${index++}`}
+      //           >
+      //             <span>{cell}</span>
+      //           </GridItem>
+      //         ));
+      //       });
+      //     });
+      //     if (board.length < 1) {
+      //       return;
+      //     }
+      //     setShades(board);
+      //   }
+      // }, [shadows]);
+
+      if (shadows) {
+        let board: JSX.Element[][][] = [];
+
+        shadows.forEach((shade, i) => {
+					console.log(`>>>>>>>>>>[${shade.player}] ${i}`);
+          board[i] = Object.entries(shade.board).map((row) => {
+            return row[1].map((cell) => (
+              <GridItem
+                style={{ background: colors[cell] }}
+                key={`cell-${index++}-${shade.player}`}
+              >
+                <span>{cell}</span>
+              </GridItem>
+            ));
+          });
+        });
+        if (board.length < 1) {
+          return;
+        }
+        setShades(board);
+      }
+    }, [shadows]);
+
+
+
+
+  // useEffect(() => {
   // 	socket.on('shadow', (board: number[][], playerName: string) => {
   //   	// console.log(`got shadow >>>>> ${board}`);
-	// 		let shade: IShadow = {
-	// 			player: playerName,
-	// 			board: board,
-	// 		};
-	// 		if (shadows.length > 0) {
+  // 		let shade: IShadow = {
+  // 			player: playerName,
+  // 			board: board,
+  // 		};
+  // 		if (shadows.length > 0) {
   //       shadows.forEach((shadow) => {
   //         if (shadow.player === playerName) {
   //           shadow = shade;
@@ -84,11 +210,10 @@ export const InfoGame = (props: IProps) => {
   //         setShadow(shadows => [...shadows, shade]);
   //       });
   //     } else {
-	// 			 shadows.push(shade);
-	// 		}
+  // 			 shadows.push(shade);
+  // 		}
   // 	});
-	// }, [])
-
+  // }, [])
 
   return (
     <>
@@ -116,12 +241,18 @@ export const InfoGame = (props: IProps) => {
           <Col>Following player</Col>
           <Col>Following player</Col>
         </Row>
-        <Row>
-          {/* <Col>{boardShadow && <MiniBoard map={boardShadow} shadows={shadows}/>}</Col> */}
-          {/* <Col>{boardShadow && <MiniBoard shadows={shadows} />}</Col> */}
-          <Col>{boardShadow && <MiniBoard />}</Col>
-        </Row>
-        <Row></Row>
+        {/* <Col>{boardShadow && <MiniBoard map={boardShadow} shadows={shadows}/>}</Col> */}
+        {/* <Col>{boardShadow && <MiniBoard shadows={shadows} />}</Col> */}
+        <Parent>
+          <Row>
+            {/* <Col>{boardShadow && <MiniBoard />}</Col>
+            <Col>{boardShadow && <MiniBoard />}</Col> */}
+            {/* <Col>{shadows && <MiniBoard />}</Col> */}
+          </Row>
+        </Parent>
+        {/* <Row>{shadows && <Grid>{shades}</Grid>}</Row> */}
+        {/* <Row>{shadows && <Grid>{shades.map((shade) => shade)}</Grid>}</Row> */}
+        <Row>{shadows && shades.map(shade => <Grid>{shade}</Grid>)}</Row>
       </Container>
     </>
   );
