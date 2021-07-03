f6c3acc4033f061574eeac8b04180629f55fdc5f
Eder De Souza <esouza@student.42.fr>
2021-07-03 12:01:19 +0200

    Update game controller

--- rtAPI/src/controller/GameController.ts
+++ rtAPI/src/controller/GameController.ts
@@ -12,53 +12,105 @@ import {
   VETERAN,
   HARDCORE,
   INSANE,
+  SHADOW,
+  UPDATE_BOARD,
 } from '../utils/const';
 
-const updatePlayers = (
+enum Mode {
+  Solo = 1,
+  Multiplayer,
+}
+
+// const updatePlayers = (
+//   io: socketIO.Server,
+//   socket: socketIO.Socket,
+//   players: Player[],
+//   gameName: string,
+//   newNextPiece: Piece
+// ): void => {
+//   players.forEach((player) => {
+//     if (player.pieceCanFall()) {
+//       // piece was already cleaned inside instance, just increment and draw() it here
+//       player.pieceIncrementY();
+//       player.draw();
+//     } else {
+//       if (player.isGameOver()) {
+//         // set player to lost
+//         player.hasLost = true;
+//         // stop game here
+//         if (gameOver(players)) {
+//           io.to(player.socketId).emit('youLost');
+//         }
+//         emitUpdatePlayerSpectrum(io, players, player);
+//       } else {
+//         // Push a new nextPiece to each user in game
+//         const nextPiece = newNextPiece.randomPiece();
+//         const points = player.scores();
+//         if (points) {
+//           io.to(player.socketId).emit('score', player.score);
+//         }
+//         players.forEach((p) => {
+//           if (points && p.name !== player.name && !p.hasLost) {
+//             p.penalty();
+//           }
+//           p.updateNextPiece = nextPiece;
+//         });
+//         //*****************************************
+//         // Set current piece to nextPiece
+//         player.getNextPiece();
+//         // Send current player nextPiece
+//         io.to(player.socketId).emit('gameInfo', player.showNextPiece);
+//         emitUpdatePlayerSpectrum(io, players, player);
+//       }
+//     }
+//     io.to(player.socketId).emit('updateBoard', player.board.shape, player.name);
+//   });
+// };
+
+/**************** START HANDLE PER PLAYER ********************** */
+
+const updatePlayerGame = (
   io: socketIO.Server,
   socket: socketIO.Socket,
   players: Player[],
+  player: Player,
   gameName: string,
-  newNextPiece: Piece
+  newNextPiece: Piece,
+  mode: Mode,
 ): void => {
-  players.forEach((player) => {
-    if (player.pieceCanFall()) {
-      // piece was already cleaned inside instance, just increment and draw() it here
-      player.pieceIncrementY();
-      player.draw();
+  if (player.pieceCanFall()) {
+    // piece was already cleaned inside instance, just increment and draw() it here
+    player.pieceIncrementY();
+    player.draw();
+  } else {
+    if (player.isGameOver()) {
+      handleGameOver(io,  players, player, mode);
+      emitUpdatePlayerShadow(io, players, player);
     } else {
-      if (player.isGameOver()) {
-        // set player to lost
-        player.hasLost = true;
-        // stop game here
-        if (gameOver(players)) {
-          io.to(player.socketId).emit('youLost');
-        }
-        emitUpdatePlayerSpectrum(io, players, player);
-      } else {
-        // Push a new nextPiece to each user in game
-        const nextPiece = newNextPiece.randomPiece();
-        const points = player.scores();
-        if (points) {
-          io.to(player.socketId).emit('score', player.score);
-        }
-        players.forEach((p) => {
-          if (points && p.name !== player.name && !p.hasLost) {
-            p.penalty();
-          }
-          p.updateNextPiece = nextPiece;
-        });
-        //*****************************************
-        // Set current piece to nextPiece
-        player.getNextPiece();
-        // Send current player nextPiece
-        io.to(player.socketId).emit('gameInfo', player.showNextPiece);
-        emitUpdatePlayerSpectrum(io, players, player);
+      // Push a new nextPiece to each user in game
+      const nextPiece = newNextPiece.randomPiece();
+      const points = player.scores();
+      if (points) {
+        io.to(player.socketId).emit('score', player.score);
       }
+      players.forEach((p) => {
+        if (points && p.name !== player.name && !p.hasLost) {
+          p.penalty();
+        }
+        p.updateNextPiece = nextPiece;
+      });
+      //*****************************************
+      // Set current piece to nextPiece
+      player.getNextPiece();
+      // Send current player nextPiece
+      io.to(player.socketId).emit('gameInfo', player.showNextPiece);
+      emitUpdatePlayerShadow(io, players, player);
     }
-    io.to(player.socketId).emit('updateBoard', player.board.shape, player.name);
-  });
+  }
+  io.to(player.socketId).emit(UPDATE_BOARD, player.board.shape, player.name);
 };
+
+/**************** END HANDLE PER PLAYER ********************** */
 
 const handlePlayerKeyDown = (
   io: socketIO.Server,
@@ -94,21 +146,34 @@ const handlePlayerKeyDown = (
   }
 };
 
-const gameOver = (players: Player[]): boolean => {
-  let playerInGame = 0;
-  players.forEach((player) => {
-    if (!player.hasLost) {
-      playerInGame++;
+const handleGameOver = (io: socketIO.Server, players: Player[], player: Player, mode: Mode): void => {
+  const losers = players.filter((p) => p.name !== player.name && p.hasLost);
+  if (players.length === 1 && mode === Mode.Solo) {
+    player.hasLost = true;
+    io.to(player.socketId).emit('youLost', { isHost: player.isHost });
+  } else if (players.length === 2) {
+   const delay = Math.floor(Math.random() * (1500 - 1 + 1) + 1);
+   setTimeout(() => {
+    if (players.length === 2) {
+      const opponent = players.find((p) => p.name !== player.name);
+      opponent.hasWon = true;
+      player.hasLost = true;
+      io.to(player.socketId).emit('youLost', { isHost: player.isHost });
+      io.to(opponent.socketId).emit('youWin', { isHost: opponent.isHost });
     }
-  });
-  return playerInGame < 2;
-};
+   }, delay)
+
+  } else {
+    player.hasLost = true;
+    io.to(player.socketId).emit('youLost', {isHost: player.isHost});
+  }
+}
 
 const emitUpdateBoard = (io: socketIO.Server, player: Player): void => {
-  io.to(player.socketId).emit('updateBoard', player.board.shape, player.name);
+  io.to(player.socketId).emit(UPDATE_BOARD, player.board.shape, player.name);
 };
 
-const emitUpdatePlayerSpectrum = (
+const emitUpdatePlayerShadow = (
   io: socketIO.Server,
   players: Player[],
   player: Player
@@ -120,7 +185,7 @@ const emitUpdatePlayerSpectrum = (
   // Send current player board shadow to all other players
   players.forEach((p) => {
     if (player.name !== p.name) {
-      io.to(p.socketId).emit('arrayOfPlayer', shadow);
+      io.to(p.socketId).emit(SHADOW, shadow);
     }
   });
 };
@@ -138,4 +203,14 @@ const gameDifficulty = (difficulty: string): number => {
   }
 };
 
-export { updatePlayers, handlePlayerKeyDown, gameDifficulty };
+const hasLost = (players: Player[], player: Player): boolean => {
+  const losers = players.filter((p) => p.name !== player.name && p.hasLost);
+  return players.length === 1 || losers.length + 1 !== players.length
+}
+
+export {
+  /* updatePlayers, */
+  updatePlayerGame,
+  handlePlayerKeyDown,
+  gameDifficulty,
+};
