2519e65bc5ce410a1742c2d9f32577068445b91c
Eder De Souza <esouza@student.42.fr>
2021-07-04 09:29:20 +0200

    Update game model

--- rtAPI/src/model/Game__.ts
+++ rtAPI/src/model/Game__.ts
@@ -11,28 +11,42 @@ type IPlayer = [
   }
 ];
 
+interface IIntervalId {
+  name: string;
+  id: NodeJS.Timeout;
+}
+
+enum Mode {
+  Solo = 1,
+  Multiplayer,
+}
+
 class Game__ {
   private _io: socketIO.Server;
   private _socket: socketIO.Socket;
   private _name: string;
   private _players: Player[];
+  private _intervalIds: IIntervalId[];
   private _piece: Piece;
   private _intervalId: NodeJS.Timeout;
   private _interval: number;
+  private _mode: Mode;
 
   constructor(
     io: socketIO.Server,
     socket: socketIO.Socket,
     players: Player[],
     gameName: string,
-    speed: number,
+    speed: number
   ) {
     this._name = gameName;
     this._io = io;
     this._socket = socket;
     this._players = players;
     this._piece = new Piece();
+    this._intervalIds = [];
     this._interval = speed;
+    this._mode = players.length === 1 ? Mode.Solo : Mode.Multiplayer;
     this.initializePieces();
     this.start();
   }
@@ -57,26 +71,55 @@ class Game__ {
     return this._players;
   }
 
+  get mode() {
+    return this._mode;
+  }
+
+  // start() {
+  //   const intervalId = setInterval(() => {
+  //     this._socket.emit('gameUpdate', 'updating');
+  //     GameController.updatePlayers(
+  //       this._io,
+  //       this._socket,
+  //       this._players,
+  //       this._name,
+  //       this._piece
+  //     );
+  //   }, this._interval);
+  //   this._intervalId = intervalId;
+  // }
+
   start() {
-    const intervalId = setInterval(() => {
-      this._socket.emit('gameUpdate', 'updating');
-      GameController.updatePlayers(
+
+    // Make each player run in it's own loop
+    this._players.forEach((player) => {
+      const interval = setInterval(() => {
+      GameController.updatePlayerGame(
         this._io,
         this._socket,
         this._players,
+        player,
         this._name,
-        this._piece
+        this._piece,
+        this._mode
       );
-    }, this._interval);
-    this._intervalId = intervalId;
+      }, this._interval);
+      const id: IIntervalId = {
+        name: player.name,
+        id: interval
+      };
+      this._intervalIds.push(id);
+    })
   }
 
   removePlayer(playerName: string) {
+    const player = this._intervalIds.find((player) => player.name === playerName);
+    clearInterval(player.id);
     const index = this._players.findIndex(
       (player) => player.name === playerName
     );
-    const removed = this._players.splice(index, 1);
-    console.log(`user was removed from game => ${removed}`);
+    const removed = this._players.splice(index, 1)[0];
+    // console.log(`user was removed from game => ${removed.name}`);
   }
 
   addNextPieceToQueu() {
@@ -84,8 +127,12 @@ class Game__ {
     this._players.forEach((player) => player.nextPiece.push(nextPiece));
   }
 
+  // stopGame() {
+  //   clearInterval(this._intervalId);
+  // }
+
   stopGame() {
-    clearInterval(this._intervalId);
+    this._intervalIds.forEach((interval) => clearInterval(interval.id))
   }
 }
 
