4736537802c7fb7ac2935641aa2f3dd494cb9a78
Eder De Souza <esouza@student.42.fr>
2021-07-09 07:01:21 +0200

    Update infoGame component

--- client/components/InfoGame.tsx
+++ client/components/InfoGame.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
-import { selectPiece } from '../features/piece/pieceSlice';
+import selectPiece from '../features/piece/pieceSlice';
 import { RootState } from '../store';
 import { connect } from 'react-redux';
 import Container from 'react-bootstrap/Container';
@@ -8,6 +8,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import styled from 'styled-components';
 import { IPiece, IShadow } from '../interfaces';
+import BoardShadow from './BoardShadow';
 import BoardGame from './BoardGame';
 import MiniBoard from './MiniBoard';
 import socket from '../utils/socket';
@@ -17,6 +18,13 @@ import {
   SHADOWS_UPDATED,
   PLAYER_SHADOW_UPDATED,
 } from '../utils/const';
+
+import {
+  nextPieceUpdated,
+  boardShadowsUpdated,
+  boardPlayerShadowUpdated,
+} from '../store/actions';
+import { PIECES } from '../../rtAPI/src/utils/const';
 
 const sizeOfGrid = 10;
 const Grid = styled.div`
@@ -30,7 +38,7 @@ const Grid = styled.div`
   max-width: 130px;
 `;
 
-const GridItem = styled.div`
+export const GridItem = styled.div`
   background-color: rgba(255, 255, 255, 0.8);
   max-height: 5px;
   max-width: 5px;
@@ -42,8 +50,8 @@ const GridItem = styled.div`
   }
 `;
 
-const Gap = styled.div<{padding: string}>`
-  padding: ${props => props.padding};
+const Gap = styled.div<{ padding: string }>`
+  padding: ${(props) => props.padding};
 `;
 
 interface IProps {
@@ -76,70 +84,65 @@ export const InfoGame = (props: IProps) => {
     Array(10).fill(0)
   );
 
-  // const [shadows, setShadows] = useState<IShadow[]>([]);
-  const [shades, setShades] = useState<JSX.Element[][][]>([]);
-  // const [players, setPlayers] = useState<IShadow[]>([]);
-  // const [nextPiece, setNextPiece] = useState<JSX.Element[][]>([]);
   let key = 0;
   const dispatch = useAppDispatch();
 
   const nextPieceState = useAppSelector((state) => state.nextPiece);
   const playersBoard = useAppSelector((state) => state.shadows);
+  const score = useAppSelector((state) => state.score);
+  const level = useAppSelector((state) => state.level);
+  const next = useAppSelector((state) => state.next);
+  const nextP = PIECES[next[0]];
 
-  nextPieceState.pos.forEach((pos) => {
-    if (pos.y < 2) {
-      nextPiece[pos.y][pos.x] = nextPieceState.color;
-    }
-  });
-
-  useEffect(() => {
-    socket.on('shaddy', () => {
-      socket.emit('getArrayOfPlayers', props.game, props.player);
+  // nextPieceState.pos.forEach((pos) => {
+  //   if (pos.y < 2) {
+  //     nextPiece[pos.y][pos.x] = nextPieceState.color;
+  //   }
+  // });
+  if (nextP) {
+    nextP.pos.forEach((pos) => {
+      if (pos.y < 2) {
+        nextPiece[pos.y][pos.x] = nextP.color;
+      }
     });
-  }, []);
+  }
 
   useEffect(() => {
     socket.on('arrayOfPlayers', (shadows: IShadow[]) => {
-      /**
-       * Need to use the object (or array) spread operator when
-       * updating deeply-nested items within state
-       * (anything deeper than the first level).
-       */
-      // shadows.forEach(shadow => console.log(`?? ${shadow.player} => ${shadow.board}`))
-      // setShadows([...shadows]);
-      dispatch({ type: SHADOWS_UPDATED, shadows: [...shadows] });
+      dispatch(boardShadowsUpdated([...shadows]));
+    });
+
+    socket.on('arrayOfPlayer', (shadow: IShadow) => {
+      dispatch(boardPlayerShadowUpdated(shadow));
+    });
+
+    socket.on('gameInfo', (nextPiece: IPiece) => {
+      dispatch(nextPieceUpdated(nextPiece));
     });
   }, []);
 
-  useEffect(() => {
-    let index = 0;
-    if (playersBoard.length > 0) {
-      let board: JSX.Element[][][] = [];
-
-      playersBoard.forEach((player, i) => {
-        // console.log(`${player.player}[${i}] ${player.board}`);
-        // console.table(player.board);
-        board[i] = player?.board?.map((row) => {
-          return(
-            row.map(cell => {
-             return (
-               <GridItem
-                 style={{ background: BOARD_COLORS[cell] }}
-                 key={`cell-${index++}-${player.player}`}
-               >
-                 <span>{cell}</span>
-               </GridItem>
-             );
-            })
-          )
-        })
-      });
-      if (board.length < 1) {
-        return;
-      }
-      setShades(board);
-    }
-  }, [playersBoard]);
+  const shadows = playersBoard.map((player, y) => {
+    return (
+      <>
+        <Col>
+          <Grid>
+            {player.board.slice(2).map((row, index) => {
+              return row.map((cell, x) => {
+                return (
+                  <GridItem
+                    style={{ background: BOARD_COLORS[cell] }}
+                    key={`${index}-${player.player}-${y}-${x}`}
+                  >
+                    <span>{cell}</span>
+                  </GridItem>
+                );
+              });
+            })}
+          </Grid>
+        </Col>
+      </>
+    );
+  });
 
   return (
     <>
@@ -148,57 +151,49 @@ export const InfoGame = (props: IProps) => {
           <Col>
             <Row>
               <Col>Player: {props.player}</Col>
-              <Col>Score:</Col>
+              <Col>Level: {level}</Col>
+              <Col>Score: {score}</Col>
             </Row>
             {
               <Grid>
-                {nextPiece.map((row) =>
+                {/* {nextPiece.map((row, i) =>
                   row.map((cell) => (
                     <GridItem
                       style={{ background: COLORS_WITH_WHITE[cell] }}
-                      key={`shaddy-next-piece-${key++}-${props.player}`}
+                      key={`shaddy-next-piece-${key++}-${props.player}-${row[i]}-${cell}`}
                     >
                       <span>{cell}</span>
                     </GridItem>
                   ))
-                )}
+                )} */}
+                {next &&
+                  nextPiece.map((row, i) =>
+                    row.map((cell) => (
+                      <GridItem
+                        style={{ background: COLORS_WITH_WHITE[cell] }}
+                        key={`shaddy-next-piece-${key++}-${props.player}-${
+                          row[i]
+                        }-${cell}`}
+                      >
+                        <span>{cell}</span>
+                      </GridItem>
+                    ))
+                  )}
               </Grid>
             }
           </Col>
         </Row>
         <Row>
-          {playersBoard?.map((player, i) => {
-            return <Col key={`player-${player.player}`}>{player.player}</Col>;
-          })}
-          
-        </Row>
-        <Row>
-          {playersBoard.map((player, y) => {
+          {playersBoard.map((player, i) => {
             return (
-              <>
-                <Col key={`shadow-board-${y}-${player.player}`}>
-                  <Grid>
-                    {player.board.map((row, i) => {
-                      return row.map((cell, index) => {
-                        return (
-                          <GridItem
-                            style={{ background: BOARD_COLORS[cell] }}
-                            key={`cell-${index++}-${player.player}${
-                              i + index + y + cell
-                            }`}
-                          >
-                            <span>{cell}</span>
-                          </GridItem>
-                        );
-                      });
-                    })}
-                  </Grid>
-                </Col>
-                {/* <Gap padding=".3em" /> */}
-              </>
+              <Col key={`player-${player.player}-${i}`}>{player.player}</Col>
             );
           })}
         </Row>
+        <Row>
+          <BoardShadow />{' '}
+        </Row>
+        {/* <Row>{shadows}</Row> */}
       </Container>
     </>
   );
