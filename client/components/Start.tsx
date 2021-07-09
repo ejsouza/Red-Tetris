ed76b044dd8f5f12c45b1f2e95a080813e2c44ba
Eder De Souza <esouza@student.42.fr>
2021-07-09 07:02:14 +0200

    Update start component

--- client/components/Start.tsx
+++ client/components/Start.tsx
@@ -3,9 +3,14 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 import Loading from './Loading';
-import { getGameByName } from '../core/games';
+import Lobby from './Lobby';
 import socket from '../utils/socket';
+import { useAppSelector, useAppDispatch } from '../store/hooks';
+import { isHostUpdated } from '../store/actions';
 
 interface IStartProps {
   gameName: string;
@@ -26,26 +31,55 @@ interface IRoom {
     {
       name: string;
       socketId: string;
+      isHost: boolean;
     }
   ];
   open: boolean;
   numberOfPlayers: number;
-  host: string;
+  host: boolean;
 }
 
 const Start = ({ gameName, playerName }: IStartProps): JSX.Element => {
   const [game, setGame] = useState<IRoom>();
+  const [difficulty, setDifficulty] = useState<string | null>('recruit');
+  const dispatch = useAppDispatch();
+  const isHost = useAppSelector((state) => state.isHost);
+  const startGame = () => {
+    socket.emit('start', gameName, difficulty);
+  };
 
+  // useEffect(() => {
 
-	const startGame = () => {
-		socket.emit('start', { name: gameName });
-	}
+  // }, []);
 
   useEffect(() => {
+    let isCancelled = false;
     socket.emit('getLobby', gameName);
     socket.on('lobby', (lobby: IRoom) => {
-      setGame(lobby);
+      const player = lobby.players.find((p) => p.name === playerName);
+      console.log(
+        `am I host ? ${player?.isHost} -- ${player?.name} - ${player?.socketId}`
+      );
+      if (player) {
+        console.log(`setting isHOst after getLobby ==> ${player.isHost}`);
+        console.log(`Name[${player.name}]`);
+        // setHost(player.isHost);
+      }
+      if (!isCancelled) {
+        setGame(lobby);
+      }
     });
+
+    socket.on('gameStart', () => {
+      console.log('Starting Game...');
+    });
+
+    socket.on('game-host', () => {
+      dispatch(isHostUpdated(true));
+    });
+    return () => {
+      isCancelled = true;
+    };
   }, []);
 
   return !game ? (
@@ -71,11 +105,64 @@ const Start = ({ gameName, playerName }: IStartProps): JSX.Element => {
       </Container>
       {console.log(game)}
       {/* This button shall be visible only for the host of the game */}
-      <Container>
-        <Button variant="secondary" size="lg" block onClick={() => startGame()}>
-          Start Game
-        </Button>
-      </Container>
+      {isHost ? (
+        <Container>
+          <div className="btn-group btn-block">
+            <Button variant="secondary" size="lg" block onClick={startGame}>
+              Start Game
+            </Button>
+
+            <DropdownButton
+              variant="secondary"
+              as={ButtonGroup}
+              title="Difficulty"
+              id="bg-nested-dropdown"
+            >
+              <Dropdown.Item
+                eventKey="recruit"
+                active={difficulty === 'recruit' ? true : false}
+                onSelect={(eventKey) => setDifficulty(eventKey)}
+              >
+                Recruit
+              </Dropdown.Item>
+              <Dropdown.Item
+                eventKey="veteran"
+                active={difficulty === 'veteran' ? true : false}
+                onSelect={(eventKey) => setDifficulty(eventKey)}
+              >
+                Veteran
+              </Dropdown.Item>
+              <Dropdown.Item
+                eventKey="hardcore"
+                active={difficulty === 'hardcore' ? true : false}
+                onSelect={(eventKey) => setDifficulty(eventKey)}
+              >
+                Hardcore
+              </Dropdown.Item>
+              <Dropdown.Item
+                eventKey="insane"
+                active={difficulty === 'insane' ? true : false}
+                onSelect={(eventKey) => setDifficulty(eventKey)}
+              >
+                Insane
+              </Dropdown.Item>
+            </DropdownButton>
+          </div>
+        </Container>
+      ) : (
+        // <Container>
+        //     <Button
+        //       variant="secondary"
+        //       size="lg"
+        //       block
+        //       onClick={() => startGame()}
+        //     >
+        //       Start Game
+        //     </Button>
+        //   </Container>
+
+        <Lobby />
+      )}
     </>
   );
 };
