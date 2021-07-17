33de3eafc959591250230a117fd93e0d1361f386
Eder De Souza <esouza@student.42.fr>
2021-07-17 07:06:02 +0200

    Update server

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,17 @@ import * as mongoose from 'mongoose';
 import 'dotenv/config';
 import { App } from './app';
 import { GamesController } from './controllers/games/games.controller';
+import { AuthController } from './controllers/auth';
+import { TokenController } from './controllers/token';
+import { UserController } from './controllers/user';
 
-const {  PORT  } = process.env;
+const { PORT } = process.env;
 
-const app = new App([new GamesController()]);
+const app = new App([
+  new GamesController(),
+  new AuthController(),
+  new TokenController(),
+  new UserController(),
+]);
 
 app.listen();
