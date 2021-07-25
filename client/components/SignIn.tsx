674f0ea5e8b090fb42b4b45b1f43d08e452cac4e
Eder De Souza <esouza@student.42.fr>
2021-07-25 06:00:47 +0200

    Upate sigin component

--- client/components/SignIn.tsx
+++ client/components/SignIn.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { signin } from '../core/user';
+import { signin, changePassword } from '../core/user';
 import { IUser } from '../interfaces';
 import { useAppDispatch } from '../store/hooks';
 import { userLoggetdUpdated } from '../store/actions';
@@ -10,6 +10,8 @@ import { PASS_REGEX, EMAIL_REGEX } from '../utils/const';
 
 const SignIn = () => {
   const [show, setShow] = useState(false);
+  const [showForgottenPassword, setShowForgottenPassword] = useState(false);
+  const [showEmailSent, setShowEmailSent] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [title, setTitle] = useState('Log-in');
@@ -22,25 +24,32 @@ const SignIn = () => {
     setShow(false);
   };
 
-  const handleSignIn = () => {
-    signin(email, password).then((logged) => {
-      const status = logged.status;
-      logged.json().then((data) => {
-        if (status === 200) {
-          const user = data as IUser;
-          if (user.token) {
-            localStorage.setItem('user', JSON.stringify(user));
-            dispatch(userLoggetdUpdated(user));
-          }
-          setEmail('');
-          setPassword('');
-          setShow(false);
-        } else {
-          setClassTitle('text-danger');
-          setTitle(data.err);
-        }
-      });
-    });
+  const handleCloseForgotten = () => {
+    setShowForgottenPassword(false);
+  };
+
+  const resetPassord = () => {
+    changePassword(email, password);
+    setShowForgottenPassword(false);
+    setShowEmailSent(true);
+    setTimeout(() => {
+      setShowEmailSent(false);
+    }, 4000);
+  };
+
+  const handleSignIn = async () => {
+    const user = await signin(email, password);
+    if (user) {
+      if (user.status === 200) {
+        const data = (await user.json()) as IUser;
+        localStorage.setItem('user', JSON.stringify(data));
+        dispatch(userLoggetdUpdated(data));
+      } else {
+        const data = await user.json();
+        setClassTitle('text-danger');
+        setTitle(data.err);
+      }
+    }
   };
 
   return (
@@ -99,10 +108,85 @@ const SignIn = () => {
           >
             Login
           </Button>
-          <Button variant="outline-secondary" block>
+          <Button
+            variant="outline-secondary"
+            block
+            onClick={() => {
+              setShow(false);
+              setShowForgottenPassword(true);
+            }}
+          >
             Forgotten your password ?
           </Button>
         </Modal.Footer>
+      </Modal>
+
+      <Modal
+        show={showForgottenPassword}
+        onHide={handleCloseForgotten}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Forgot password</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            Please enter the <b>email</b> linked to your account, and a{' '}
+            <b>new password</b>, we will reset your password and send a
+            confirmation link to your inbox. Make sure to check it as soon as
+            possible, the link will expire within one hour.
+          </p>
+          <hr />
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                isInvalid={EMAIL_REGEX.test(email) ? false : true}
+                type="email"
+                placeholder="Enter email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>New password</Form.Label>
+                <Form.Control
+                  isInvalid={PASS_REGEX.test(password) ? false : true}
+                  type="password"
+                  placeholder="Enter a new password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={resetPassord}
+            block
+            disabled={
+              !PASS_REGEX.test(password) || !EMAIL_REGEX.test(email)
+                ? true
+                : false
+            }
+          >
+            Confirm
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={showEmailSent} backdrop="static" keyboard={false}>
+        <Modal.Header>
+          <Modal.Title>Reset password request sent</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Please check your<b>&nbsp;email&nbsp;</b>
+          box, we sent you a link for reseting your password. This link will
+          expire within one hour.
+        </Modal.Body>
       </Modal>
     </>
   );
