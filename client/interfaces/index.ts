3cd6ed0bb61b464a5d8aabe5eba02fd3bd010fcb
Eder De Souza <esouza@student.42.fr>
2021-07-25 06:00:20 +0200

    Upate interfaces

--- client/interfaces/index.ts
+++ client/interfaces/index.ts
@@ -9,6 +9,12 @@ export type User = {
   name: string
 }
 
+interface IUser {
+  success: boolean;
+  id: string;
+  token: string;
+}
+
 interface IShadow {
   player: string;
   board: number[][];
@@ -33,4 +39,4 @@ interface IBoard {
   board: number[][];
 }
 
-export type { IPiece, IInitialState, IBoard, IShadow };
+export type { IPiece, IInitialState, IBoard, IShadow, IUser };
