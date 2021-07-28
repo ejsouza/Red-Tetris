62fa67e735c5ecb46121c9f23677023eb2dfe00c
Eder De Souza <esouza@student.42.fr>
2021-07-28 07:12:45 +0200

    Update constants

--- server/src/config/const.ts
+++ server/src/config/const.ts
@@ -1,3 +1,24 @@
-export const BaseURL = '/api/v1';
+const BaseURL = '/api/v1';
+const TEST_BASE_URL = 'http://localhost:8000/api/v1';
+export const FRONT_URL: string = 'http://localhost:3000';
 
-export const MAX_NUMBER_OF_PLAYERS = 3;
+const MAX_NUMBER_OF_PLAYERS = 4;
+
+const SALT_ROUNDS = 10;
+
+const SECRET_TOKEN = 'thisIsASecreatToken';
+
+const emailCheck =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+const PASS_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+
+export {
+  BaseURL,
+  SALT_ROUNDS,
+  MAX_NUMBER_OF_PLAYERS,
+  SECRET_TOKEN,
+  emailCheck,
+  PASS_REGEX,
+  TEST_BASE_URL,
+};
