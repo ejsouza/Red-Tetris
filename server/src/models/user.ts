2ee5927b3963f0204e582557ad48e3750f0a157c
Eder De Souza <esouza@student.42.fr>
2021-07-29 07:24:31 +0200

    Update user model

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,50 +1,56 @@
 import mongoose from 'mongoose';
 import { IUser } from '../interfaces/';
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    unique: [true, 'This email is already in use'],
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: [true, 'This email is already in use'],
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      default: '',
+    },
+    lastName: {
+      type: String,
+      default: '',
+    },
+    playedGames: {
+      type: Number,
+      default: 0,
+    },
+    victory: {
+      type: Number,
+      default: 0,
+    },
+    defeat: {
+      type: Number,
+      default: 0,
+    },
+    bestScore: {
+      type: Number,
+      default: 0,
+    },
+    bestLevel: {
+      type: Number,
+      default: 0,
+    },
+    status: {
+      type: String,
+      default: 'pending',
+    },
+    token: {
+      type: String,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  firstName: {
-    type: String,
-  },
-  lastName: {
-    type: String,
-  },
-  playedGames: {
-    type: Number,
-    default: 0,
-  },
-  bestScore: {
-    type: Number,
-    default: 0,
-  },
-  bestLevel: {
-    type: Number,
-    default: 0,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-  status: {
-    type: String,
-    default: 'pending',
-  },
-  token: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
+
 
 export default mongoose.model<IUser>('User', userSchema);
