d4fe82b16742291cf9fec2aae1052b31997c25e7
Eder De Souza <esouza@student.42.fr>
2021-08-05 07:34:00 +0200

    Create game tests

---
+++ rtAPI/src/controller/game.spec.ts
@@ -0,0 +1,21 @@
+let chaI = require('chai');
+let chaiHttp = require('chai-http');
+let assert = require('assert');
+import { gameDifficulty } from './GameController';
+
+chaI.use(chaiHttp);
+
+describe('Game Controller', () => {
+  it('should return 420 (recruit)', () => {
+    assert.equal(gameDifficulty('recruit'), (700 * 60) / 100);
+  });
+  it('should return 300 (veteran)', () => {
+    assert.equal(gameDifficulty('veteran'), (500 * 60) / 100);
+  });
+  it('should return 180 (hardcore)', () => {
+    assert.equal(gameDifficulty('hardcore'), (300 * 60) / 100);
+  });
+  it('should return 60 (insane)', () => {
+    assert.equal(gameDifficulty('insane'), (100 * 60) / 100);
+  });
+});
