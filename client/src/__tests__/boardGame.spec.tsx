c03c15c76073e5c0bac3e6253a201020a9915797
Eder De Souza <esouza@student.42.fr>
2021-08-23 07:43:13 +0200

    Create boardGame tests

---
+++ client/src/__tests__/boardGame.spec.tsx
@@ -0,0 +1,51 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import * as React from 'react';
+import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import BoardGame from '../components/BoardGame';
+import configureStore from 'redux-mock-store';
+import { BOARD_HEIGHT, BOARD_WIDTH, EMPTY_PIECE } from '../utils/const';
+import { IShadow } from '../interfaces';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      number: React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
+  }
+}
+
+const intialShadows: IShadow[] = [];
+const initialNext: number[] = [];
+let mockStore = configureStore([]);
+
+describe('Component BoardGame()', () => {
+  let store = mockStore({
+    board: Array.from({ length: BOARD_HEIGHT }, () =>
+      Array(BOARD_WIDTH).fill(0)
+    ),
+    piece: EMPTY_PIECE[0],
+    nextPiece: EMPTY_PIECE[0],
+    next: initialNext,
+    shadows: intialShadows,
+    score: 0,
+    level: 0,
+    isHost: false,
+  });
+
+  const component = render(
+    <Provider store={store}>
+      <BoardGame />
+    </Provider>
+  );
+
+  it('should test BoardGame', () => {
+    expect(component.asFragment());
+  });
+});
