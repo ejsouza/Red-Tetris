c7cfdb8729351627140884bf924f45b0615bbc2b
Eder De Souza <esouza@student.42.fr>
2021-08-28 08:09:03 +0200

    Create playerDashBoard  tests

---
+++ client/src/__tests__/playerDashBoard.spec.tsx
@@ -0,0 +1,46 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import * as React from 'react';
+import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import configureStore from 'redux-mock-store';
+import PlayerDashBoard from '../components/PlayerDashBoard';
+import { BOARD_HEIGHT, BOARD_WIDTH, EMPTY_PIECE } from '../utils/const';
+import { IShadow, IUser } from '../interfaces';
+
+const initialUser: IUser = {
+  success: false,
+  id: 'randomId42',
+  token: 'qwertyuiopasdfghjklzxcvbnm123456789',
+};
+
+const intialShadows: IShadow[] = [];
+const initialNext: number[] = [];
+let mockStore = configureStore([]);
+
+describe('Component PlayerDashBoard()', () => {
+  let store = mockStore({
+    board: Array.from({ length: BOARD_HEIGHT }, () =>
+      Array(BOARD_WIDTH).fill(0)
+    ),
+    piece: EMPTY_PIECE[0],
+    nextPiece: EMPTY_PIECE[0],
+    next: initialNext,
+    shadows: intialShadows,
+    score: 0,
+    level: 0,
+    isHost: true,
+    user: initialUser,
+  });
+
+  it('should match Next Piece', () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <PlayerDashBoard />
+      </Provider>
+    );
+    expect(getByText('Next Piece')).toBeInTheDocument();
+  });
+});
