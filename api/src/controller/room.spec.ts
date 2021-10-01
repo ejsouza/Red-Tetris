df5c7de25645d42db719291aa6ae53cade9239a0
Eder De Souza <esouza@student.42.fr>
2021-10-01 09:01:29 +0200

    Update room controller tests

--- api/src/controller/room.spec.ts
+++ api/src/controller/room.spec.ts
@@ -16,53 +16,53 @@ chaI.use(chaiHttp);
 
 describe('Room Controller', () => {
   let io, serverSocket, clientSocket;
-  // beforeEach((done) => {
-  //   const httpServer = createServer();
-  //   io = new Server(httpServer);
-  //   httpServer.listen(() => {
-  //     const port = httpServer.address().port;
-  //     clientSocket = new Client(`http://localhost:${port}`);
-  //     io.on('connection', (socket) => {
-  //       serverSocket = socket;
-  //     });
-  //     clientSocket.on('connect', done);
-  //   });
-  // });
+  beforeEach((done) => {
+    const httpServer = createServer();
+    io = new Server(httpServer);
+    httpServer.listen(() => {
+      const port = httpServer.address().port;
+      clientSocket = new Client(`http://localhost:${port}`);
+      io.on('connection', (socket) => {
+        serverSocket = socket;
+      });
+      clientSocket.on('connect', done);
+    });
+  });
 
-  // afterEach(() => {
-  //   io.close();
-  //   clientSocket.close();
-  // });
+  afterEach(() => {
+    io.close();
+    clientSocket.close();
+  });
 
   it('should create room', (done) => {
-    // let room1 = createRoom('room42', 'player42', '424242');
-    // chaI.expect(room1.isOpen).to.be.true;
-    // chaI.expect(room1.name).to.be.equal('room42');
+    let room1 = createRoom('room42', 'player42', '424242');
+    chaI.expect(room1.isOpen).to.be.true;
+    chaI.expect(room1.name).to.be.equal('room42');
     done();
   });
 
-  // it('should add player to room', (done) => {
-  //   let room2 = createRoom('room2', 'player2', '222222');
-  //   addPlayerToRoom(room2, 'toto', '212121');
-  //   chaI.expect(room2.players.length).to.be.equal(2);
-  //   done();
-  // });
+  it('should add player to room', (done) => {
+    let room2 = createRoom('room2', 'player2', '222222');
+    addPlayerToRoom(room2, 'toto', '212121');
+    chaI.expect(room2.players.length).to.be.equal(2);
+    done();
+  });
 
-  // it('should emit success created (join-room)', (done) => {
-  //   clientSocket.on('join-room', (arg) => {
-  //     assert.equal(arg.msg, `Please wait joining loby`);
-  //   });
-  //   emitSuccesfullyCreated(serverSocket, true, true);
-  //   done();
-  // });
+  it('should emit success created (join-room)', (done) => {
+    clientSocket.on('join-room', (arg) => {
+      assert.equal(arg.msg, `Please wait joining loby`);
+    });
+    emitSuccesfullyCreated(serverSocket, true, true);
+    done();
+  });
 
-  // it('should emit room is full', (done) => {
-  //   clientSocket.on('room', (arg) => {
-  //     assert.equal(arg.msg, `This room 'roomFull' is full`);
-  //   });
-  //   emitRoomIsFull(serverSocket, 'roomFull', false);
-  //   done();
-  // });
+  it('should emit room is full', (done) => {
+    clientSocket.on('room', (arg) => {
+      assert.equal(arg.msg, `This room 'roomFull' is full`);
+    });
+    emitRoomIsFull(serverSocket, 'roomFull', false);
+    done();
+  });
 
   // /**
   //  * To check the message `This username 'toto' is already taken`
@@ -71,11 +71,11 @@ describe('Room Controller', () => {
   //  * Because it is emting 'room' or 'join-room' two listener in the
   //  * same event is conflicting.
   //  */
-  // it('should emit username is taken', (done) => {
-  //   clientSocket.on('room', (arg) => {
-  //     assert.equal(arg.msg, `This username 'toto' is already taken`);
-  //   });
-  //   emitUserNametaken(serverSocket, 'toto', false);
-  //   done();
-  // });
+  it('should emit username is taken', (done) => {
+    clientSocket.on('room', (arg) => {
+      assert.equal(arg.msg, `This username 'toto' is already taken`);
+    });
+    emitUserNametaken(serverSocket, 'toto', false);
+    done();
+  });
 });
