e383165bafc1432376900457b9b964f13c3e6be1
Eder De Souza <esouza@student.42.fr>
2021-10-01 09:11:39 +0200

    Update player tests

--- api/src/model/player.spec.ts
+++ api/src/model/player.spec.ts
@@ -23,48 +23,51 @@ describe('Player Model', () => {
     board.shape[2][0] = 1;
   });
 
-  it('should return piece', () => {
+  it('should return piece', (done) => {
     chaI.expect(player.piece.shape.color).to.be.equal(1);
+    done();
   });
 
-  it('should set player lost', () => {
+  it('should set player lost', (done) => {
     player.lost = true;
-  });
-
-  it('should player lost', () => {
     chaI.expect(player.lost).to.be.equal(true);
+    done();
   });
 
-  it('should set game lost At', () => {
+  it('should set game lost At', (done) => {
     player.gameLostAt = Date.now();
+    done();
   });
 
-  it('should get game lost At', () => {
+  it('should get game lost At', (done) => {
     let now = Date.now();
     chaI.expect(player.gameLostAt).to.be.at.lessThanOrEqual(now);
+    done();
   });
 
-  it('should set is host', () => {
+  it('should set is host', (done) => {
     player.isHost = true;
-  });
-
-  it('should be host', () => {
     chaI.expect(player.isHost).to.be.equal(true);
+    done();
   });
 
-  it('should reset board', () => {
+  it('should reset board', (done) => {
     player.resetBoard();
+    done();
   });
 
-  it('should update board shape', () => {
+  it('should update board shape', (done) => {
     player.boardShape = board.shape;
+    done();
   });
 
-  it('should return third row reached (true)', () => {
+  it('should return third row reached (true)', (done) => {
     chaI.expect(player.isThirdRowReached()).to.be.equal(true);
+    done();
   });
 
-  it('should return toObject', () => {
+  it('should return toObject', (done) => {
     chaI.expect(player.toObject().score).to.be.at.equal(0);
+    done();
   });
 });
